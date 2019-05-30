767ed2b30f2d684e4c9c848017327a9b25c9d0c9
team awesome <maxj@email.arizona.edu>
2019-05-29 22:25:40 -0700

    gitignore, server updates, views/updates templates, stylesheet

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 //dependencies
-var express = require("express");
+var express = require('express');
+var mongoose = require('mongoose');
+var expressHandlebars = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 //dynamic port
 var PORT = process.env.PORT || 3000;
@@ -11,10 +14,36 @@ var app = express();
 var router = express.Router();
 
 //public folder is a static directory
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
+
+//connect Handlebars to Express
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view-engine', 'handlebars');
+
+//body parser use in application
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 //middleware for every request to go through
 app.use(router);
+
+//use deployed db or the local mongoHeadlines db
+var db = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+//connect to db
+mongoose.connect(db, function(error) {
+    //log errors
+    if (error) {
+        console.log(error);
+    }
+    //or log success
+    else {
+        console.log('mongoose connection is successful');
+    }
+});
 
 //Listen on a port
 app.listen(PORT, function() {
