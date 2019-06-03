ced08e1e6ba199e4634baaa2b60a9403a13e9148
team awesome <maxj@email.arizona.edu>
2019-06-03 00:22:30 -0700

    delete, get, & update functions written for headlines.js

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -10,7 +10,7 @@ module.exports = {
     // use fetch to run a function, pass cb into that function, then run scrape
     fetch: function(cb) {
         scrape(function(data){
-            
+
             // set data to be articles, go thru ea. article and run makeDate function and saved to false
             var articles = data;
             for (var i=0; i < articles.length; i++) {
@@ -23,5 +23,25 @@ module.exports = {
                 cb(err, docs);
             });
         });
+    },
+    // wrote a delete property 
+    delete: function(query, cb) {
+        Headline.remove(query, cb);
+    },
+    //wrote a way to get the items out of the collection, sort from newest to oldest, then pass the docs to a cb function
+    get: function(query, cb) {
+        Headline.find(query)
+        .sort({
+            _id: -1
+        })
+        .exec(function(err, doc){
+            cb(doc);
+        });
+    },
+    //function to update the scraped articles with a relevant id & info
+    update: function(query, cb) {
+        Headline.update({_id: query._id}, {
+            $set: query
+        }, {}, cb);
     }
 }
