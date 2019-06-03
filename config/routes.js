06b33485b99c378ceeafa34e371bd05088e449af
team awesome <maxj@email.arizona.edu>
2019-06-03 10:02:30 -0700

    get headlines, del article, updt headlines, del notes, post notes

--- config/routes.js
+++ config/routes.js
@@ -18,7 +18,7 @@ module.exports = function(router) {
     // fetch route for an API route to get articles
     router.get('/api/fetch', function(req, res){
         headlinesController.fetch(function(err, docs){
-            if (!docs || docs.insertedCount === 0) {
+            if (!docs || docs.insertedCount === 0) { 
                res.jason ({
                    message: "There are no new articles today. Please check again tomorrow!"
                }); 
@@ -30,6 +30,59 @@ module.exports = function(router) {
             }
         });
     });
+    // grab headlines in the db
+    router.get('/api/headlines', function(req, cb){
+        var query = {};
+        if (req.query.saved) {
+            query = req.query;
+        }
 
-    
+        headlinesController.get(query, function(data){
+            res.json(data);
+        });
+    });
+
+    //route for deleting a specific article
+    router.delete('/api/headlines/:id', function(){
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function(){
+            res.json(data);
+        });
+    });
+
+    // route to update headlines as needed
+    router.patch('/api/headlines', function(req, res){
+        headlinesController.update(req.body, function(err, data){
+            res.json(data);
+        });
+    });
+
+    // route to grab notes assoc. with article for user
+    router.get('api/notes/:headline_id', function(req, res){
+        var query = {};
+        if (req.params.headline_id) {
+            query._id = req.params.headline_id;
+        }
+
+        notesController.get(query, function(err, data){
+            res.json(data);
+        });
+    });
+
+    //route to delete notes 
+    router.delete('/api/notes/:id', function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        notesController.delete(query, function(err, data){
+            res.jason(data);
+        });
+    });
+
+    //route to post new notes to articles
+    router.post('api/notes', function(req, res){
+        notesController.save(req.body, function(data){
+            res.jason(data);
+        });
+    });
 }
