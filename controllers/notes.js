02b58d4327edcf10a1fa2cb52f2a847791e36457
team awesome <maxj@email.arizona.edu>
2019-06-03 08:49:46 -0700

    completed controller for notes; get/save/delete func.

--- controllers/notes.js
+++ controllers/notes.js
@@ -4,5 +4,34 @@ var Note = require('../models/Note');
 var nameDate = require('../scripts/date');
 
 module.exports = {
-    
+
+    // not beginning with a scrape function; begin with a get function & then create a save function
+    get: function(data, cb) {
+        Note.find({
+            _headlineId: data._id
+        }, cb);
+    },
+    save: function(data, cb) {
+        var newNote = {
+            _headlineId: data._id,
+            data: makeDate(),
+            noteText: data.noteText
+        };
+
+        Note.create(newNote, function (err, doc) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(doc);
+                cb(doc);
+            }
+        });
+    },
+    // create a delete function
+    delete: function(data, cb) {
+        Note.remove({
+            _id: data._id
+        }, cb);
+    }
 }
